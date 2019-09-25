5c96b6889ea8dba4dae59d5cf11c9ad66144adc8
tmackiewicz <tmackiewicz@unity-t.pl>
2019-09-25 13:43:05 +0200

    Lexik Event Listener

---
+++ api/src/Event/Listener/JWTCreatedListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Event\Listener;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    /**
+     * Adds additional data to the generated JWT
+     *
+     * @param JWTCreatedEvent $event
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $user = $event->getUser();
+        $payload = array_merge(
+            $event->getData(),
+            [
+                'userId' => $user->getId()
+            ]
+        );
+        $event->setData($payload);
+    }
+}
