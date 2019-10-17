e37a57dbc8e98dda22fb3bc0186a7cdc5932b7c7
tmackiewicz <tmackiewicz@unity-t.pl>
2019-10-17 13:47:28 +0200

    Admin sidenav

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { NavService } from './menu-list-item/nav.service';
+import { NavItem } from './menu-list-item/model/nav-item';
+import { NAV_ITEMS } from './menu-list-item/const/nav-items';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements AfterViewInit {
+    @ViewChild('sidenav', {static: false}) sidenav: ElementRef;
+    isLoggedIn: boolean = true;
+    title = 'client';
+    sidenavOpened: boolean = true;
+    navItems: NavItem[] = NAV_ITEMS;
+
+    constructor(
+        private navService: NavService
+    ) {}
+
+    ngAfterViewInit() {
+        this.navService.sidenav = this.sidenav;
+    }
 }
