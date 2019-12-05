da8b4df3de4f5a8562da0ab89585723486156f02
tmackiewicz <tmackiewicz@unity-t.pl>
2019-12-05 12:12:47 +0100

    Handle login errors

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,25 +1,32 @@
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { AuthenticationService } from '../common/services/authentication.service';
 import { User } from '../user/model/user';
+import { AlertService } from '../common/services/alert.service';
+import { handleError } from '../common/functions/error.functions';
 
 @Component({
     selector: 'login',
     templateUrl: './login.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush       
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
     user: User = new User();
     
     constructor(
         private authenticationService: AuthenticationService,
-        private ref: ChangeDetectorRef
+        private ref: ChangeDetectorRef,
+        private alertService: AlertService
     ) {}
 
     login() {
         this.authenticationService.login(this.user.username, this.user.password);
         this.authenticationService.loginError.subscribe(
-            (error) => {
-                //this.ref.detectChanges();
+            (error: any) => {
+                let errors = handleError(error);
+                if (errors !== null && typeof errors.message !== 'undefined') {
+                    this.alertService.openSnackBar(errors.message, 'error-notification-overlay');
+                }
+                this.ref.detectChanges();
             }
         );
     }
